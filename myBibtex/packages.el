bc835f70abeab38fdae44cab2cafcf1ba5eedf4f
Sanghyuk Park <sanghyuk.park85@gmail.com>
2018-06-19 22:54:56 -0500

    Fix the issue of opening up notes

--- myBibtex/packages.el
+++ myBibtex/packages.el
@@ -12,15 +12,44 @@
 (defconst myBibtex-packages
       '(
         auctex
+        helm-bibtex
         org
         org-ref
         markdown-mode
-        helm-bibtex
         ))
 
 (defun myBibtex/post-init-auctex ()
   (spacemacs/set-leader-keys-for-major-mode 'latex-mode
     "ic" 'org-ref-helm-insert-cite-link))
+
+(defun myBibtex/init-helm-bibtex ()
+  (use-package helm-bibtex
+    :commands (helm-bibtex)
+    :init
+    (progn
+      (spacemacs/set-leader-keys "ob" 'helm-bibtex)
+      (setq helm-bibtex-full-frame nil
+            bibtex-completion-bibliography '("~/Dropbox/Bibliography/references.bib")
+            bibtex-completion-notes-path "~/Dropbox/Bibliography/Notes/"
+            bibtex-completion-library-path '("~/Dropbox/Bibliography/Papers/"))
+      (setq bibtex-completion-pdf-open-function
+            (lambda (fpath)
+              (call-process "open" nil 0 nil "-a" "/Applications/Skim.app" fpath)))
+      (setq bibtex-completion-pdf-symbol "f")
+      (setq bibtex-completion-notes-symbol "n")
+      (setq bibtex-completion-display-formats
+            '((t . "${=type=:7}: ${author: 20} (${year:4}) ${title:*} :${=has-pdf=:1}${=has-note=:1}:"))))
+    :config
+    (setq helm-bibtex-notes-template-multiple-files
+          (format
+           "#+TITLE: Notes on: ${title}\n#+INTERLEAVE_PDF: ~/Dropbox/Bibliography/Papers/${=key=}.pdf\n#+PUB_AUTHORS:\t${author}\n#+PUB_YEAR: \t${year}\n\n"))))
+
+(defun myBibtex/post-init-helm-bibtex ()
+  (with-eval-after-load 'helm-bibtex
+    (progn
+      (require 'org-ref)
+      (setq bibtex-completion-display-formats
+            '((t . "${=type=:7}: ${author: 20} (${year:4}) ${title:*} :${=has-pdf=:1}${=has-note=:1}:"))))))
 
 (defun myBibtex/post-init-org ()
   (spacemacs/set-leader-keys-for-major-mode 'org-mode
@@ -28,24 +57,23 @@
 
 (defun myBibtex/init-org-ref ()
   (use-package org-ref
-    :commands (org-ref-helm-insert-cite-link
-               org-ref-bibtex-next-entry
-               org-ref-bibtex-previous-entry
-               org-ref-open-in-browser
-               org-ref-open-bibtex-notes
-               org-ref-open-bibtex-pdf
-               org-ref-bibtex-hydra/body
-               org-ref-bibtex-hydra/org-ref-bibtex-new-entry/body-and-exit
-               org-ref-sort-bibtex-entry
-               arxiv-add-bibtex-entry
-               arxiv-get-pdf-add-bibtex-entry
-               doi-utils-add-bibtex-entry-from-doi
-               isbn-to-bibtex
-               pubmed-insert-bibtex-from-pmid)
+    :defer t
+    ;; :commands (org-ref-helm-insert-cite-link
+    ;;            org-ref-bibtex-next-entry
+    ;;            org-ref-bibtex-previous-entry
+    ;;            org-ref-open-in-browser
+    ;;            org-ref-open-bibtex-notes
+    ;;            org-ref-open-bibtex-pdf
+    ;;            org-ref-bibtex-hydra/body
+    ;;            org-ref-bibtex-hydra/org-ref-bibtex-new-entry/body-and-exit
+    ;;            org-ref-sort-bibtex-entry
+    ;;            arxiv-add-bibtex-entry
+    ;;            arxiv-get-pdf-add-bibtex-entry
+    ;;            doi-utils-add-bibtex-entry-from-doi
+    ;;            isbn-to-bibtex
+    ;;            pubmed-insert-bibtex-from-pmid)
     :init
     (progn
-      (setq bibtex-completion-display-formats
-            '((t . "${=type=:7}: ${author: 20} (${year:4}) ${title:*} :${=has-pdf=:1}${=has-note=:1}:")))
       (evil-define-key 'normal bibtex-mode-map
         (kbd "C-j") 'org-ref-bibtex-next-entry
         (kbd "C-k") 'org-ref-bibtex-previous-entry
@@ -72,46 +100,22 @@
         "lA" 'arxiv-get-pdf-add-bibtex-entry
         "ld" 'doi-utils-add-bibtex-entry-from-doi
         "li" 'isbn-to-bibtex
-        "lp" 'pubmed-insert-bibtex-from-pmid)
-    (setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
-    (setq org-ref-default-bibliography '("~/Dropbox/bibliography/references.bib")
-          org-ref-pdf-directory "~/Dropbox/bibliography/papers/"
-          org-ref-bibliography-notes "~/Dropbox/Bibliography/Notes/")
-    (setq org-ref-notes-function
-          (lambda (thekey)
-            (let ((bibtex-completion-bibliography (org-ref-find-bibliography)))
-              (bibtex-completion-edit-notes
-               (list (car (org-ref-get-bibtex-key-and-file thekey)))))))
-    (setq org-ref-open-pdf-function
-          (lambda (fpath)
-            (call-process "open" nil 0 nil "-a" "/Applications/Skim.app" fpath)))
-    )))
-
-(defun myBibtex/post-init-org-ref ()
-  (setq bibtex-completion-display-formats
-        '((t . "${=type=:7}: ${author: 20} (${year:4}) ${title:*} :${=has-pdf=:1}${=has-note=:1}:"))))
+        "lp" 'pubmed-insert-bibtex-from-pmid))
+    :config
+    (progn
+      (setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
+      (setq org-ref-default-bibliography '("~/Dropbox/bibliography/references.bib")
+            org-ref-pdf-directory "~/Dropbox/bibliography/papers/"
+            org-ref-notes-directory "~/Dropbox/Bibliography/Notes/")
+      (setq org-ref-notes-function
+            (lambda (thekey)
+              (let ((bibtex-completion-bibliography (org-ref-find-bibliography)))
+                (bibtex-completion-edit-notes
+                 (list (car (org-ref-get-bibtex-key-and-file thekey)))))))
+      (setq org-ref-open-pdf-function
+            (lambda (fpath)
+              (call-process "open" nil 0 nil "-a" "/Applications/Skim.app" fpath))))))
 
 (defun myBibtex/post-init-markdown-mode ()
   (spacemacs/set-leader-keys-for-major-mode 'markdown-mode
     "ic" 'org-ref-helm-insert-cite-link))
-
-(defun myBibtex/init-helm-bibtex ()
-  (use-package helm-bibtex
-    :init
-    (progn
-      (spacemacs/set-leader-keys "ob" 'helm-bibtex)
-      (setq reftex-default-bibliography '("~/Dropbox/bibliography/references.bib"))
-      (setq helm-bibtex-full-frame nil
-            bibtex-completion-bibliography '("~/Dropbox/Bibliography/references.bib")
-            bibtex-completion-note-path '("~/Dropbox/Bibliography/Notes/")
-            bibtex-completion-library-path '("~/Dropbox/Bibliography/Papers/"))
-      (setq bibtex-completion-pdf-open-function
-            (lambda (fpath)
-              (call-process "open" nil 0 nil "-a" "/Applications/Skim.app" fpath)))
-      (setq helm-bibtex-notes-template-multiple-files
-            (format
-             "#+TITLE: Notes on: ${title}\n#+INTERLEAVE_PDF: ~/Dropbox/Bibliography/Papers/${=key=}.pdf\n#+PUB_AUTHORS:\t${author}\n#+PUB_YEAR: \t${year}\n\n"))
-      (setq bibtex-completion-pdf-symbol "f")
-      (setq bibtex-completion-notes-symbol "n")
-      (setq bibtex-completion-display-formats
-            '((t . "${=type=:7}: ${author: 20} (${year:4}) ${title:*} :${=has-pdf=:1}${=has-note=:1}:"))))))
