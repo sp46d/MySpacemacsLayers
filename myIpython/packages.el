7f9be66c1111ee1e4c56bb1acdba36d4c1138395
Sanghyuk Park <sanghyuk.park85@gmail.com>
2018-06-22 19:40:00 -0500

    Move Scimax path out to config.el

--- myIpython/packages.el
+++ myIpython/packages.el
@@ -16,10 +16,6 @@
         (ox-ipynb :location local)
         ))
 
-(defun myIpython/pre-init-ob-ipython ()
-  (setq scimax-dir "path/to/scimax")
-  (add-to-list 'load-path "path/to/scimax"))
-
 (defun myIpython/init-ob-ipython ()
     (use-package ob-ipython
       :defer t
@@ -33,16 +29,8 @@
   (spacemacs|use-package-add-hook org
     :post-config (add-to-list 'org-babel-load-languages '(ipython . t))))
 
-;; (defun myIpython/post-init-ob-ipython ()
-;;   (with-eval-after-load 'org
-;;     (progn
-;;       (require 'scimax-org-babel-ipython-upstream)
-;;       (require 'ob-ipython))))
-
 (defun myIpython/init-ox-ipynb ()
   (use-package ox-ipynb
     :defer t
     :init
     (with-eval-after-load 'org (require 'ox-ipynb))))
-
-
